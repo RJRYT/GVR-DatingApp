fb738b34d00623311e5e070bb9270017c5698cf1
RJRYT <robinjratr@gmail.com>
2024-07-27 03:03:32 +0530

    baseurl updated

--- frontend/src/Instance/Axios.js
+++ frontend/src/Instance/Axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+const getBaseUrl = ()=>{
+  const hostname = window.location.hostname;
+  alert(hostname);
+  if(hostname.startsWith('192.168.')){
+    return `http://${hostname}:5000/api`;
+  }
+  return `${process.env.REACT_APP_API_URL}/api`;
+};
+
 const apiInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api", // Your API base URL
+  baseURL: getBaseUrl(), //process.env.REACT_APP_API_URL + "/api", // Your API base URL
   withCredentials: true,
 });
 
